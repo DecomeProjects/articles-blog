2801ed40f4aeb6403822ce5462ab25f9c1cfc79f
Felipe Decome <decome.felipe@gmail.com>
2022-07-06 17:45:58 -0300

    chore: initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Home | Articles Blog</title>
+      </Head>
+      
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default Home
