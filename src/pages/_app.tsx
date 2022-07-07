eff17d6ced80ea48040caaa238d69a93fd11f9c7
Felipe Decome <decome.felipe@gmail.com>
2022-07-07 17:31:57 -0300

    feat: home page ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import '../styles/global.scss';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Header />
+
       <Component {...pageProps} />
     </SessionProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
-
-import { Header } from '../components/Header';
+import Image from 'next/image';
+import styles from './home.module.scss';
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +10,46 @@ const Home: NextPage = () => {
         <title>Home | Articles Blog</title>
       </Head>
 
-      <Header />
+      <div className={styles.homeContainer}>
+        <main>
+          <span>
+            <Image
+              width={26}
+              height={26}
+              src="/images/clapping.png"
+              alt="clapping hands"
+            />
+            Hey, welcome
+          </span>
+
+          <div>
+            <h2>
+              News about
+              <br />
+              the <span className={styles['text-blue']}>React</span> world
+            </h2>
+
+            <p>
+              Get acess to all the publications
+              <br />
+              <span className={styles['text-blue']}>for $9,90 month</span>
+            </p>
+          </div>
+
+          <button type="button">Subscribe now</button>
+        </main>
+
+        <div className={styles.image}>
+          <div>
+            <Image
+              width={668}
+              height={1040}
+              src="/images/woman-coding.svg"
+              alt="woman coding"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
